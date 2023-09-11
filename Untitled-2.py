68cbbd25900382242d5a34e988857478e4ab97b9
“[BlogCH]” <[chriatiangeovanniarredondorang@gmail.com]>
2023-09-11 11:56:13 -0600

    Calculadora

---
+++ Untitled-2.py
@@ -0,0 +1,18 @@
+'''
+Autor: Arredondo Rangel Christian Geovanni 
+Fecha:
+Descripcion Iniciando python 
+'''
+
+a = input("Ingresa el primer valor:")
+b = input("Ingresa el segundo valor:")
+print (type(a), type(b))
+a = int (a)
+b = int (b)
+print (type(a), type(b))
+
+print ("Resta: ", a - b)
+print("Suma: ", a+b)
+print ("Suma de numeros reales: ", a + b)
+print ("Division: ", a / b)
+print ("Multiplicacion: ", a*b)
