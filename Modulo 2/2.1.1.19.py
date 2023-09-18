54f48d862dce61a0a49dd88094ca99f12ee2454a
Christian Arredondo <144488426+BlogCH@users.noreply.github.com>
2023-09-18 11:36:17 -0600

    Add files via upload

--- Modulo 2/2.1.1.19.py
+++ Modulo 2/2.1.1.19.py
@@ -1,7 +1,20 @@
-﻿﻿'''
+'''
     Nombre: Vhristian Geovanni Arredondo Rangel
     Fecha 08/19/2023
     Descripción: Laboratorio 2.1.1.18
 '''
-
+print("Original")
+print("    *")
+print("   * *")
+print("  *   *")
+print(" *     *")
+print("***   ***")
+print("  *   *")
+print("  *   *")
+print("  *****")
+print("Minimizar el número de invocaciones de la función print() insertando la secuencia \n en las cadenas.")
+print("    *\n   * *\n  *   *\n *     *\n***   ***\n  *   *\n  *   *\n  *****")
+print("Hacer la flecha dos veces más grande")
+print("    *    \n   * *   \n  *   *  \n *     * \n***   ***\n  *   *  \n  *   *  \n  *****  ")
+print("Duplicar la flecha, colocando ambas flechas lado a lado; nota: una cadena se puede multiplicar usando el siguiente truco")
 print("    *    "*2,"   * *   "*2,"  *   *  "*2," *     * "*2,"***   ***"*2,"  *   *  "*2,"  *   *  "*2,"  *****  "*2, sep="\n") 
