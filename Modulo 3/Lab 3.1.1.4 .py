5de061d90f59b9074af32d947bb0377bf1c38efe
Christian Arredondo <144488426+BlogCH@users.noreply.github.com>
2023-09-25 09:48:20 -0600

    Add files via upload
    Lab modulo 3

---
+++ Modulo 3/Lab 3.1.1.4 .py
@@ -0,0 +1,8 @@
+﻿'''
+    autor: Christian Geovanni Arredondo Rangel}
+    Fechar: 5 septiembre 2023
+    Descripción :3.1.1.4 LABORATORIO: Preguntas y respuestas
+
+'''
+n= int( input("Ingresa un dato: "))
+print( n >= 100) #imprime tru o false 
