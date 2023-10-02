79b4c04bd8a26903d9a663d2405122d16af5245c
Christian Arredondo <144488426+BlogCH@users.noreply.github.com>
2023-10-01 20:56:37 -0600

    Add files via upload

---
+++ Modulo 3/LAB 3.1.1.4.py
@@ -0,0 +1,23 @@
+﻿'''
+    Autor: Christian Geovanni Arredondo Rangel 
+    Descipción: Lab 3.1.1.4
+    Fecha: 01/10/2023
+'''
+
+n=50==1 #Primera  False
+print(n) 
+
+n= 99!=99 #Segunda False
+print(n) 
+
+n=100<900   #Tercera  True
+print(n) 
+
+n=101<=102  #cuarta    True
+print(n)
+
+n= -5>9  #Quinta     false
+print(n)
+
+n =+123>=100 #Sexta  true
+print(n)
