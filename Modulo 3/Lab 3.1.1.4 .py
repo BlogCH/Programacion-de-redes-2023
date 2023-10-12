8a134d7477b38b3b6b176b4e519b85f25dfad1fc
Christian Arredondo <144488426+BlogCH@users.noreply.github.com>
2023-10-12 12:32:11 -0600

    Delete Modulo 3/Lab 3.1.1.4 .py

--- Modulo 3/Lab 3.1.1.4 .py
+++
@@ -1,8 +0,0 @@
-﻿'''
-    autor: Christian Geovanni Arredondo Rangel}
-    Fechar: 5 septiembre 2023
-    Descripción :3.1.1.4 LABORATORIO: Preguntas y respuestas
-
-'''
-n= int( input("Ingresa un dato: "))
-print( n >= 100) #imprime tru o false 
