5590a6258f5f6674dab317e927d458dfd07560a8
BlogCH <chriatiangeovanniarredondorang@gmail.com>
2023-11-11 16:00:25 -0600

    Cambios a ser confirmados: 	borrados:        Modulo 2/2.1.1.16.py 	borrados:        Modulo 2/2.1.1.18.py 	borrados:        Modulo 2/2.1.1.19.py 	borrados:        Modulo 2/LAB 2.2.1.11.py 	borrados:        Modulo 2/LAB 2.4.1.10.py 	borrados:        Modulo 2/LAB 2.4.1.7.py 	borrados:        Modulo 2/LAB 2.4.1.9.py 	borrados:        Modulo 2/LAB 2.5.1.2.py 	borrados:        Modulo 2/LAB 2.6.1.10.py 	borrados:        Modulo 2/LAB 2.6.1.11.py 	borrados:        Modulo 2/LAB 2.6.1.9.py 	borrados:        Modulo 2/Labs 2.1.1.18.pdf 	borrados:        Modulo 2/Labs 2.1.1.19.pdf 	borrados:        Modulo 2/Labs 2.1.1.6.pdf 	borrados:        Modulo 3/Lab 3.1.1.10.py 	borrados:        Modulo 3/Lab 3.1.1.11.py 	borrados:        Modulo 3/Lab 3.1.1.12.py 	borrados:        Modulo 3/Lab 3.1.1.4.py 	borrados:        Modulo 3/Lab 3.2.1.10.py 	borrados:        Modulo 3/Lab 3.2.1.11.py 	borrados:        Modulo 3/Lab 3.2.1.14.py 	borrados:        Modulo 3/Lab 3.2.1.15.py 	borrados:        Modulo 3/Lab 3.2.1.3.py 	borrados:        Modulo 3/Lab 3.2.1.6.py 	borrados:        Modulo 3/Lab 3.2.1.9.py 	borrados:        Modulo 3/Lab 3.4.1.13.py 	borrados:        Modulo 3/Lab 3.4.1.6.py 	borrados:        Modulo 3/Lab 3.6.1.9.py 	borrados:        Modulo 3/UI_R1_Introduccion_IdC.pdf 	borrados:        Modulo 4/Lab 4.3.1.10.py 	borrados:        Modulo 4/Lab 4.3.1.6.py 	borrados:        Modulo 4/Lab 4.3.1.7.py 	borrados:        Modulo 4/Lab 4.3.1.8.py 	borrados:        Modulo 4/Lab 4.3.1.9.py 	borrados:        Modulo 4/Lab 4.7.2.1.py 	borrados:        Modulo 4/Plantilla.py 	borrados:        unidad1_modulo1/Problema 1.py 	borrados:        unidad1_modulo1/Problema 2.py 	borrados:        unidad1_practicov2/u1_prob1.py 	borrados:        unidad1_practicov2/u1_prob2.py

--- Modulo 2/2.1.1.18.py
+++
@@ -1,7 +0,0 @@
-﻿'''
-    Nombre: Vhristian Geovanni Arredondo Rangel
-    Fecha 08/19/2023
-    Descripción: Laboratorio 2.1.1.18
-'''
-print("Fundamentos","Programación","en",sep="***", end="...")
-print("Python")
--- Modulo 4/Plantilla.py
+++
@@ -1,5 +0,0 @@
-'''
-Autor : Christian Geovanni Arredondo Rangel
-Fecha 11/10/2023
-Descriocion: Lab 4.3.1.8
-'''
